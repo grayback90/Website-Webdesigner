84d040f063ca566c2c268456f32b6ad5da24c990
Kenny Maeder <kenny-maeder@web.de>
2022-07-13 12:47:35 +0200

    little changes on about and css for testimonials

--- src/components/About.js
+++ src/components/About.js
@@ -1,40 +1,42 @@
 import React from "react";
 //CSS
-import "./About.css"
+import "./About.css";
+//Images
+import coding_logo from "./pictures/website_images/coding.svg";
 
 function About() {
     return (
         <section id="about">
-          <div className="container">
-            <div className="flex">
-              <h1 className="h1_about">
+          <div className="about_container">
+            <div className="about_flex">
+              <h1 className="about_h1">
                 Hi, I'm Reed.
-                <br className="hidden" />I love to build amazing
+                <br className="abouthidden" />I love to build amazing
                 apps.
               </h1>
               <p className="about_me">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui
-                laborum quasi, incidunt dolore iste nostrum cupiditate voluptas?
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. <br></br>
+                Qui laborum quasi, incidunt dolore iste nostrum cupiditate voluptas?<br></br>
                 Laborum, voluptas natus?
               </p>
-              <div className="flex_center">
+              <div className="about_flex_center">
                 <a
                   href="#contact"
-                  className="link_contact">
+                  className="about_link_contact">
                   Work With Me
                 </a>
                 <a
                   href="#projects"
-                  className="link_projects">
+                  className="about_link_projects">
                   See My Past Work
                 </a>
               </div>
             </div>
-            <div className="coding_icon">
+            <div className="about_coding_icon">
               <img
-                className="coding"
+                className="about_coding"
                 alt="hero"
-                src="./pictures/website_images/coding.svg"
+                src={coding_logo}
               />
             </div>
           </div>
