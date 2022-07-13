32b69337b11bca21e63db3e9a5642f0967ee422e
Kenny Maeder <kenny-maeder@web.de>
2022-07-13 15:30:56 +0200

    contact css

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -30,8 +30,8 @@ function Contact() {
 
     return (
         <section id="contact" className="relative">
-          <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
-            <div className="lg:w-2/3 md:w-1/2 bg-gray-900 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
+          <div className="contact_container">
+            <div className="contact_map">
               <iframe
                 width="100%"
                 height="100%"
@@ -43,27 +43,27 @@ function Contact() {
                 style={{ filter: "opacity(0.7)" }}
                 src="https://www.google.com/maps/embed/v1/place?q=Eilenburg&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"
               />
-              <div className="bg-gray-900 relative flex flex-wrap py-6 rounded shadow-md">
-                <div className="lg:w-1/2 px-6">
-                  <h2 className="title-font font-semibold text-white tracking-widest text-xs">
+              <div className="contact_outer_adress">
+                <div className="contact_inner_adress">
+                  <h2 className="contact_h2">
                     ADDRESS
                   </h2>
-                  <p className="mt-1">
+                  <p className="contact_country">
                     04838 Eilenburg <br />
                     Sachsen, Deutschland
                   </p>
                 </div>
-                <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
-                  <h2 className="title-font font-semibold text-white tracking-widest text-xs">
+                <div className="contact_mail">
+                  <h2 className="contact_h2">
                     EMAIL
                   </h2>
-                  <a className="text-indigo-400 leading-relaxed">
+                  <a className="contact_mail_link">
                     reedbarger@email.com
                   </a>
-                  <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
+                  <h2 className="contact_h2">
                     PHONE
                   </h2>
-                  <p className="leading-relaxed">123-456-7890</p>
+                  <p className="contact_phone_number">123-456-7890</p>
                 </div>
               </div>
             </div>
@@ -71,54 +71,54 @@ function Contact() {
               netlify
               name="contact"
               onSubmit={handleSubmit}
-              className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
-              <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
+              className="contact_form">
+              <h2 className="contact_form_h2">
                 Hire Me
               </h2>
-              <p className="leading-relaxed mb-5">
+              <p className="contact_hire_text">
                 Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum
                 suscipit officia aspernatur veritatis. Asperiores, aliquid?
               </p>
-              <div className="relative mb-4">
-                <label htmlFor="name" className="leading-7 text-sm text-gray-400">
+              <div className="contact_outer_label">
+                <label htmlFor="name" className="contact_label_name">
                   Name
                 </label>
                 <input
                   type="text"
                   id="name"
                   name="name"
-                  className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                  className="contact_input"
                   onChange={(e) => setName(e.target.value)}
                 />
               </div>
-              <div className="relative mb-4">
-                <label htmlFor="email" className="leading-7 text-sm text-gray-400">
+              <div className="contact_outer_label">
+                <label htmlFor="email" className="contact_label">
                   Email
                 </label>
                 <input
                   type="email"
                   id="email"
                   name="email"
-                  className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                  className="contact_input"
                   onChange={(e) => setEmail(e.target.value)}
                 />
               </div>
-              <div className="relative mb-4">
+              <div className="contact_outer_label">
                 <label
                   htmlFor="message"
-                  className="leading-7 text-sm text-gray-400">
+                  className="contact_label">
                   Message
                 </label>
                 <textarea
                   id="message"
                   name="message"
-                  className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+                  className="contact_input_message"
                   onChange={(e) => setMessage(e.target.value)}
                 />
               </div>
               <button
                 type="submit"
-                className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+                className="contact_send_button">
                 Submit
               </button>
             </form>
