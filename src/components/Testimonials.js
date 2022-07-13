311184cb71a64a8eb00f94f9787499d6deb15a49
Kenny Maeder <kenny-maeder@web.de>
2022-07-13 13:04:18 +0200

    filled testimonials css

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -1,32 +1,34 @@
 import { TerminalIcon, UsersIcon } from "@heroicons/react/solid";
 import React from "react";
 import { testimonials } from "../data";
+//CSS
+import "./Testimonials.css"
 
 function Testimonials() {
     return (
         <section id="testimonials">
-          <div className="container px-5 py-10 mx-auto text-center">
-            <UsersIcon className="w-10 inline-block mb-4" />
-            <h1 className="sm:text-4xl text-3xl font-medium title-font text-white mb-12">
+          <div className="testimonials_container">
+            <UsersIcon className="testimonials_usericon" />
+            <h1 className="testimonials_h1">
               Client Testimonials
             </h1>
-            <div className="flex flex-wrap m-4">
+            <div className="testimonials_flex">
               {testimonials.map((testimonial) => (
-                <div className="p-4 md:w-1/2 w-full">
-                  <div className="h-full bg-gray-800 bg-opacity-40 p-8 rounded">
-                    <TerminalIcon className="block w-8 text-gray-500 mb-4" />
-                    <p className="leading-relaxed mb-6">{testimonial.quote}</p>
-                    <div className="inline-flex items-center">
+                <div className="testimonials_outer">
+                  <div className="testimonials_inner">
+                    <TerminalIcon className="testimonials_terminalicon" />
+                    <p className="testimonials_quote">{testimonial.quote}</p>
+                    <div className="testimonials_inlineflex">
                       <img
                         alt="testimonial"
                         src={testimonial.image}
-                        className="w-12 rounded-full flex-shrink-0 object-cover object-center"
+                        className="testimonials_images"
                       />
-                      <span className="flex-grow flex flex-col pl-4">
-                        <span className="title-font font-medium text-white">
+                      <span className="testimonials_outer_name">
+                        <span className="testimonials_name">
                           {testimonial.name}
                         </span>
-                        <span className="text-gray-500 text-sm uppercase">
+                        <span className="testimonials_company">
                           {testimonial.company}
                         </span>
                       </span>
